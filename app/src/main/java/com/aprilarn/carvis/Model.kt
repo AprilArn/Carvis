53659840ac51896f5fdd787b2eb76e16f08bd4b3
Arnold <urjelarnoldb@gmail.com>
2025-06-10 11:45:56 +0700

    changing model to 640_float16.tflite

--- app/src/main/java/com/aprilarn/carvis/Model.kt
+++ app/src/main/java/com/aprilarn/carvis/Model.kt
@@ -1,6 +1,6 @@
 package com.aprilarn.carvis
 
 object Model {
-    const val MODEL_PATH = "736_float16.tflite"
+    const val MODEL_PATH = "640_float16.tflite"
     val LABEL_PATH: String? = "labels.txt"
 }
